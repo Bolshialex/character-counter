f47ead8e4c45dc44cbd183503e7f1d522b495efb
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-01-20 13:09:15 -0800

    Finished code along JUint Testing

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,23 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld(){
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello world");
+        // Act
+        int actualCount = counter.getFrequency('l');
+        // Assert
+        assertEquals(3,actualCount);
+    }
+
+    @Test
+    public void testZAppearsZeroPercentInString(){
+        
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+
+        double percentCount = counter.getRelativePercentage('z');
+
+        assertEquals(0.0, percentCount, 0.0001);
+    }
 }
